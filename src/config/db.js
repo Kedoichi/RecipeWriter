d811706e03c6c36f96bf02c803422c3c4f590ce9
Vũ Tiến Quốc <42103573+Kedoichi@users.noreply.github.com>
2024-11-03 14:44:52 +1100

    Deploy Mongo

--- src/config/db.js
+++ src/config/db.js
@@ -3,26 +3,33 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = process.env.MONGODB_URI; // Use the environment variable
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true, // Ensure TLS is enabled
+  tlsInsecure: false, // Set to true for testing purposes only
 });
 
 async function run() {
   try {
-    // Connect the client to the server (optional starting in v4.7)
+    // Connect the client to the server
     await client.connect();
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
 
-run().catch(console.dir);
+run().catch(console.error);
